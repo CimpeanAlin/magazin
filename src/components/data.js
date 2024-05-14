6be50e331333a8116be273c37d7262200c1d312f
Cimpean Alin <alin.cimpean1@stud.ubbcluj.ro>
2024-05-14 18:00:20 +0300

    first commit

---
+++ src/components/data.js
@@ -0,0 +1,44 @@
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.wall-art.de/format:webp/q:92/rs:fit:1560:/_img/out/pictures/master/product/1/wanddeko-holz-pappel-furnier-hirsch-im-wald-web-01.jpg",
+    title: "Lemn",
+  },
+  {
+    id: 2,
+    img: "https://i.pinimg.com/236x/15/5f/6d/155f6d81537b423f1b9b3a47768d76fe.jpg",
+    title: "Metal",
+  },
+  {
+    id: 3,
+    img: "https://i.pinimg.com/564x/32/5d/21/325d2186de453e89598b1db1dc3fa889.jpg",
+    title: "Sticla",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://cloud2.bcdn.ro/Uploads/item/2023/05/11/05/16837839607759938-item.jpg?width=310&aspect_ratio=4:5&resize_mode=stretch&016837839607759938",
+  },
+  {
+    id: 2,
+    img: "https://cloud2.bcdn.ro/Uploads/item/2024/04/24/08/17139473548115941-item.jpg?width=310&aspect_ratio=4:5&resize_mode=stretch&017139473548115941",
+  },
+  {
+    id: 3,
+    img: "https://cloud2.bcdn.ro/Uploads/item/2023/07/13/06/16892284657803803-item.jpg?width=310&aspect_ratio=4:5&resize_mode=stretch&016892284657803803",
+  },
+  {
+    id: 4,
+    img: "https://cloud2.bcdn.ro/Uploads/item/2023/05/27/13/16851957157774026-item.jpg?width=310&aspect_ratio=4:5&resize_mode=stretch&016851957157774026",
+  },
+  {
+    id: 5,
+    img: "https://cloud2.bcdn.ro/Uploads/item/2024/05/06/12/17149974998121490-item.jpg?width=310&aspect_ratio=4:5&resize_mode=stretch&017149974998121490",
+  },
+  {
+    id: 6,
+    img: "https://cloud2.bcdn.ro/Uploads/item/2020/03/30/14/15855801236053231-item.jpg?width=310&aspect_ratio=4:5&resize_mode=stretch&015855801236053231",
+  },
+];
