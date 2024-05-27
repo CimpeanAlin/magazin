97192a9a330d9899fa3c976ac5baa507356ce695
Cimpean Alin <alin.cimpean1@stud.ubbcluj.ro>
2024-05-27 14:32:32 +0300

    Added dashboard HTML

--- API/routes/users.js
+++ API/routes/users.js
@@ -1,9 +1,21 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const Order = require("../models/Order");
+const { verifyTokenAndAuthorization } = require("./verifyToken");
 
-router.get("/usertest", (req, res) => {
-    res.send("user test sucess");
-});
+// Get user details and orders
+router.get(
+  "/dashboard/:userId",
+  verifyTokenAndAuthorization,
+  async (req, res) => {
+    try {
+      const user = await User.findById(req.params.userId).select("-password"); // Exclude password
+      const orders = await Order.find({ userId: req.params.userId });
+      res.status(200).json({ user, orders });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+);
 
 module.exports = router;
-
-
--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './pages/Home';
-import ProductList from './pages/ProductList';
-import Product from './pages/Product';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Cart from './pages/Cart';
-import Hero from './components/Hero';
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Hero from "./components/Hero";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from './redux/store';
-
+import { store } from "./redux/store";
 
 const App = () => {
   return (
-
-   
     <Provider store={store}>
       <Router>
         <Routes>
@@ -25,11 +28,11 @@ const App = () => {
           <Route path="/product/:id" element={<Product />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/dashboard/:userId" element={<Dashboard />} />
           <Route path="/register" element={<Register />} />
         </Routes>
       </Router>
     </Provider>
-
   );
 };
 
