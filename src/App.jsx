203c20374650c3073da1eda6bdb3cd9d3a4d7025
Cimpean Alin <alin.cimpean1@stud.ubbcluj.ro>
2024-05-27 14:46:50 +0300

    Provider fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,33 @@
-
-import logo from './logo.svg';
-import './App.css';
-import Home from './pages/Home';
-import ProductList from './pages/ProductList';
-import Product from './pages/Product';
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
 import Dashboard from "./pages/Dashboard";
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Cart from './pages/Cart';
-import Blog from './pages/Blog';
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Blog from "./pages/Blog";
+import { store } from "./redux/store";
+import "./App.css";
 
 const App = () => {
   return (
     <Provider store={store}>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products/:category" element={<ProductList />} />
-        <Route path="/product/:id" element={<Product />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products/:category" element={<ProductList />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/dashboard/:userId" element={<Dashboard />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/blog" element={<Blog />} />
-      </Routes>
-    </Router>
-</Provider>
+          <Route path="/register" element={<Register />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 };
 
